f3084113fa60d8a6e3551b7eac44a154b72102b6
Charles B Johnson <mail@cbjohnson.info>
2014-08-19 17:29:40 -0400

    Stop using nvm and ndenv.

--- bashrc
+++ bashrc
@@ -6,7 +6,3 @@ alias ls='ls --color=auto'
 # use rbenv for ruby
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-
-# use ndenv for node
-export PATH="$HOME/.ndenv/bin:$PATH"
-eval "$(ndenv init -)"
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,7 @@ set fish_theme agnoster
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins bak gi rbenv ndenv vi-mode
+set fish_plugins bak gi rbenv vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
