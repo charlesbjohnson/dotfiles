9c97b3f812b439dffce985bff621a101c9ed09bc
Charles B Johnson <mail@cbjohnson.info>
2014-08-29 16:51:49 -0400

    Better git alias and new fish plugin.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,7 @@ set fish_theme agnoster
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins bak gi rbenv nodenv vi-mode
+set fish_plugins bak extract gi rbenv nodenv vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
