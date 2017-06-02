e417f1a863b2b69ce50c7bbf83345d24428261d2
Charles B Johnson <mail@cbjohnson.info>
2017-06-02 14:17:19 -0400

    Implement shell functions for fish.

---
+++ config/shell/init.fish
@@ -0,0 +1,29 @@
+function shell::termcolor
+  if not string match --quiet "256color" $TERM
+    set --export --global TERM $argv[1]
+  end
+end
+
+function shell::color
+  if test -f "$HOME/.config/shell/$argv[1]"
+    eval sh "$HOME/.config/shell/$argv[1]"
+  end
+end
+
+function shell::dircolors
+  if not test -f "$HOME/.config/shell/$argv[1]"
+    return
+  end
+
+  set try_cmds "dircolors" "gdircolors"
+  set cmd
+
+  for try_cmd in $try_cmds
+    if command --search --quiet $try_cmd
+      set cmd $try_cmd
+      break
+    end
+  end
+
+  bass (eval $cmd "$HOME/.config/shell/$argv[1]")
+end
