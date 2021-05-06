8022408ea5110345f8c67ebfbbe6fc8833743a1c
Charles B Johnson <mail@cbjohnson.info>
2021-05-06 10:47:37 -0500

    Prune plugin settings

--- host-abaddon/config/nvim/lua/init/overrides/local.lua
+++ host-abaddon/config/nvim/lua/init/overrides/local.lua
@@ -6,30 +6,18 @@ vim.g.splitjoin_ruby_hanging_args         = 0
 vim.g.splitjoin_ruby_options_as_arguments = 1
 
 -- dense-analysis/ale
-vim.g.ale_fixers["css"] = {"stylelint"}
-vim.g.ale_linters["css"] = {"stylelint"}
-
-vim.g.ale_fixers["scss"] = g.ale_fixers["css"]
-vim.g.ale_linters["scss"] = g.ale_linters["css"]
-
-vim.g.ale_fixers["javascript"] = {"xo"}
-vim.g.ale_fixers["javascriptreact"] = g.ale_fixers["javascript"]
-
 vim.g.ale_javascript_eslint_suppress_missing_config = 1
+vim.g.ale_fixers["javascript"]  = {"xo"}
+vim.g.ale_fixers["typescript"]  = {"xo"}
+vim.g.ale_linters["javascript"] = {"xo"}
+vim.g.ale_linters["typescript"] = {"xo"}
 
-vim.g.ale_linters["javascript"] = g.ale_fixers["javascript"]
-vim.g.ale_linters["javascriptreact"] = g.ale_fixers["javascript"]
-
-vim.g.ale_fixers["ruby"] = {"rubocop"}
-vim.g.ale_linters["ruby"] = {"rubocop", "sorbet"}
 vim.g.ale_ruby_rubocop_executable = "bundle"
-vim.g.ale_ruby_sorbet_executable = "bundle"
+vim.g.ale_fixers["ruby"]          = {"rubocop"}
+vim.g.ale_linters["ruby"]         = {"rubocop"}
 
-vim.g.ale_fixers["rust"] = {"rustfmt"}
+vim.g.ale_fixers["rust"]  = {"rustfmt"}
 vim.g.ale_linters["rust"] = {"cargo", "rustc"}
 
-vim.g.ale_fixers["sh"] = {"shfmt"}
+vim.g.ale_fixers["sh"]  = {"shfmt"}
 vim.g.ale_linters["sh"] = {"shellcheck"}
-
-vim.g.ale_fixers["typescript"] = g.ale_fixers["javascript"]
-vim.g.ale_linters["typescript"] = g.ale_fixers["typescript"]
