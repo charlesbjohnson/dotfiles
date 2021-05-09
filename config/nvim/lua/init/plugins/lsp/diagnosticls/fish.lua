63f2405631166ffb979ec98a4de1b5b7501b6281
Charles B Johnson <mail@cbjohnson.info>
2021-05-09 18:57:36 -0500

    Configure fish linting and formatting

---
+++ config/nvim/lua/init/plugins/lsp/diagnosticls/fish.lua
@@ -0,0 +1,29 @@
+return function(setup)
+  table.insert(setup.filetypes, "fish")
+
+  setup.init_options.filetypes.fish = "fish"
+  setup.init_options.linters.fish = {
+    sourceName = "fish",
+
+    command = "fish",
+    args    = {"--no-execute", "%file"},
+
+    debounce = 100,
+    isStdout = false,
+    isStderr = true,
+
+    formatLines   = 1,
+    formatPattern = {
+      [[^.*\(line (\d+)\): (.*)$]],
+      {
+        line    = 1,
+        message = 2
+      }
+    }
+  }
+
+  setup.init_options.formatFiletypes.fish = "fish_indent"
+  setup.init_options.formatters.fish_indent = {
+    command = "fish_indent"
+  }
+end
