c350da1a9fc2105a37b1e592825fc192bdac6ce2
Charles B Johnson <mail@cbjohnson.info>
2021-05-11 16:35:08 -0500

    Restructure lsp plugins

---
+++ host-abaddon/config/nvim/lua/init/plugins/lsp/diagnosticls/local.lua
@@ -0,0 +1,10 @@
+return function(setup)
+  require("plugins.lsp.diagnosticls.fish")(setup)
+  require("plugins.lsp.diagnosticls.sh")(setup)
+
+  require("plugins.lsp.diagnosticls.javascript")(setup)
+  require("plugins.lsp.diagnosticls.typescript")(setup)
+
+  require("plugins.lsp.diagnosticls.ruby")(setup)
+end
+
