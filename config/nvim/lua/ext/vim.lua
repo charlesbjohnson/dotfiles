602a470b8a1438195c2ce7067f0ff92b7b8a681d
Charles B Johnson <mail@cbjohnson.info>
2021-05-14 00:41:24 -0500

    Add lua extension for setting highlights

--- config/nvim/lua/ext/vim.lua
+++ config/nvim/lua/ext/vim.lua
@@ -30,4 +30,42 @@ function M.mapping(mode)
   end
 end
 
+function M.highlight(name, options)
+  local definitions = {}
+
+  if options.bg then
+    options.guibg = options.bg
+    options.bg = nil
+  end
+
+  if options.fg then
+    options.guifg = options.fg
+    options.fg = nil
+  end
+
+  if options.style then
+    options.gui = options.style
+    options.style = nil
+  end
+
+  for k, v in pairs(options or {}) do
+    table.insert(definitions, string.join({k, v}, "="))
+  end
+
+  vim.cmd(
+    string.join(
+      {
+        "highlight",
+        name,
+        string.join(definitions, " "),
+      },
+      " "
+    )
+  )
+end
+
+function M.highlight_link(from, to)
+  vim.cmd(string.join({"highlight", "link", from, to}, " "))
+end
+
 return M
