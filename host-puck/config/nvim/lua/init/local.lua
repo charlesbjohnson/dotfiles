371a6ba015fc6ba5a126849994fa4eec504fc532
Charles B Johnson <mail@cbjohnson.info>
2021-05-27 12:51:09 -0500

    Configure new host

---
+++ host-puck/config/nvim/lua/init/local.lua
@@ -0,0 +1,6 @@
+-- AndrewRadev/splitjoin.vim
+vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+
+vim.g.splitjoin_ruby_do_block_split       = 0
+vim.g.splitjoin_ruby_hanging_args         = 0
+vim.g.splitjoin_ruby_options_as_arguments = 1
---
+++ host-puck/config/nvim/lua/init/plugins/lsp/ruby.lua
@@ -0,0 +1,1 @@
+return require("plugins.lsp.ruby")
