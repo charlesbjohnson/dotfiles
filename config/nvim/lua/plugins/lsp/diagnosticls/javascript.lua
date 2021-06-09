a2bee2e42d919aa487d8975e7ca8b867e8e95f6c
Charles B Johnson <mail@cbjohnson.info>
2021-06-08 23:39:35 -0500

    Switch to eslint from xo

--- config/nvim/lua/plugins/lsp/diagnosticls/javascript.lua
+++ config/nvim/lua/plugins/lsp/diagnosticls/javascript.lua
@@ -2,14 +2,20 @@ return function(setup)
   table.insert(setup.filetypes, "javascript")
   table.insert(setup.filetypes, "typescript")
 
-  setup.init_options.filetypes.javascript = "xo"
-  setup.init_options.filetypes.typescript = "xo"
+  table.insert(setup.filetypes, "javascriptreact")
+  table.insert(setup.filetypes, "typescriptreact")
 
-  setup.init_options.linters.xo = {
-    sourceName = "xo",
+  setup.init_options.filetypes.javascript = "eslint"
+  setup.init_options.filetypes.typescript = "eslint"
+
+  setup.init_options.filetypes.javascriptreact = "eslint"
+  setup.init_options.filetypes.typescriptreact = "eslint"
+
+  setup.init_options.linters.eslint = {
+    sourceName = "eslint",
 
     command      = "npm",
-    args         = {"exec", "--yes", "--parseable", "--", "xo", "--reporter", "json", "--stdin", "--stdin-filename", "%filepath"},
+    args         = {"exec", "--yes", "--parseable", "--", "eslint_d", "--format", "json", "--stdin", "--stdin-filename", "%filepath"},
     rootPatterns = {"package.json", ".git"},
 
     debounce = 100,
@@ -32,12 +38,15 @@ return function(setup)
     }
   }
 
-  setup.init_options.formatFiletypes.javascript = "xo"
-  setup.init_options.formatFiletypes.typescript = "xo"
+  setup.init_options.formatFiletypes.javascript = "eslint"
+  setup.init_options.formatFiletypes.typescript = "eslint"
 
-  setup.init_options.formatters.xo = {
-    command      = "npm",
-    args         = {"exec", "--yes", "--parseable",  "--", "xo", "--fix", "--stdin", "--stdin-filename", "%filepath"},
+  setup.init_options.formatFiletypes.javascriptreact = "eslint"
+  setup.init_options.formatFiletypes.typescriptreact = "eslint"
+
+  setup.init_options.formatters.eslint = {
+    command      = "sh",
+    args         = {"-c", "npm exec --yes --parseable -- eslint_d --stdin --stdin-filename '%filepath' --fix-to-stdout"},
     rootPatterns = {"package.json", ".git"},
   }
 end
