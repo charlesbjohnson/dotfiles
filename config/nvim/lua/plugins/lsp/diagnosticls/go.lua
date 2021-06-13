6c14ad5abac6d256bbd01472449c5d21d6de6958
Charles B Johnson <mail@cbjohnson.info>
2021-06-13 11:31:16 -0500

    Add lint configuration for go

---
+++ config/nvim/lua/plugins/lsp/diagnosticls/go.lua
@@ -0,0 +1,25 @@
+return function(setup)
+  table.insert(setup.filetypes, "go")
+
+  setup.init_options.filetypes.go = "golangci"
+  setup.init_options.linters.golangci = {
+    sourceName = "golangci",
+
+    command      = "golangci-lint",
+    args         = {"run", "--out-format", "json"},
+    rootPatterns = {"go.mod", ".git"},
+
+    debounce = 100,
+
+    parseJson = {
+      sourceName       = "Pos.Filename",
+      sourceNameFilter = true,
+
+      errorsRoot = "Issues",
+      message    = "(${FromLinter}) ${Text}",
+
+      line   = "Pos.Line",
+      column = "Pos.Column"
+    }
+  }
+end
