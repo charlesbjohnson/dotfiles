9b6ed334c188a7cff0c3e262d0bef8c7c48243e2
Charles B Johnson <mail@cbjohnson.info>
2022-01-20 14:24:50 -0600

    Update shell colors

--- config/fish/functions/fish_nord_colors.fish
+++ config/fish/functions/fish_nord_colors.fish
@@ -3,18 +3,27 @@ function fish_nord_colors
     set --global fish_color_cancel -r
     set --global fish_color_command brblue
     set --global fish_color_comment brblack
+    set --global fish_color_cwd green
+    set --global fish_color_cwd_root red
     set --global fish_color_end cyan
     set --global fish_color_error red
     set --global fish_color_escape brcyan
+    set --global fish_color_history_current --bold
+    set --global fish_color_host normal
+    set --global fish_color_keyword brblue
+    set --global fish_color_match --background=brblue
     set --global fish_color_normal normal
     set --global fish_color_operator brcyan
+    set --global fish_color_option normal
     set --global fish_color_param normal
     set --global fish_color_quote green
     set --global fish_color_redirection magenta
-    set --global fish_color_search_match --background=brblack
+    set --global fish_color_search_match bryellow --background=brblack
     set --global fish_color_selection white --bold --background=brblack
+    set --global fish_color_user brgreen
     set --global fish_color_valid_path --underline
 
+    set --global fish_pager_color_completion normal
     set --global fish_pager_color_description brcyan
     set --global fish_pager_color_prefix --underline
     set --global fish_pager_color_progress white --background=brblue
