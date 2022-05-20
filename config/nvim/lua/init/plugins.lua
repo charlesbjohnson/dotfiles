9ba48d4457022aa84be7ec90a608268065b8e30b
Charles B Johnson <mail@cbjohnson.io>
2022-05-19 22:44:04 -0500

    Load colorscheme plugin first

--- config/nvim/lua/init/plugins.lua
+++ config/nvim/lua/init/plugins.lua
@@ -1,5 +1,37 @@
 local colors = require("colors")
 
+-- shaunsingh/nord.nvim
+vim.g.nord_borders = true
+vim.g.nord_contrast = true
+vim.g.nord_italic = false
+
+require("nord").set()
+
+-- TODO: submit fix for yellow
+vim.api.nvim_set_hl(0, "BufferCurrentMod", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "BufferInactiveMod", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "BufferVisibleMod", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "DiffChange", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "diffChanged", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "GitSignsChange", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "GitSignsChangeLn", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "GitSignsChangeNr", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultWarning", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "LspDiagnosticsFloatingWarning", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "LspDiagnosticsSignWarning", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "LspDiagnosticsUnderlineWarning", { sp = colors[13] })
+vim.api.nvim_set_hl(0, "LspDiagnosticsVirtualTextWarning", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "WarningMsg", { fg = colors[13] })
+vim.api.nvim_set_hl(0, "Warnings", { fg = colors[13] })
+
+vim.api.nvim_set_hl(0, "healthWarning", { fg = colors[13] })
+
 -- AndrewRadev/splitjoin.vim
 vim.g.splitjoin_quiet = 1
 
@@ -281,33 +313,6 @@ vim.keymap.set("n", "<Leader>L", "<Cmd>BufferMoveNext<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>b", "<Cmd>enew<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>h", "<Cmd>BufferPrevious<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>l", "<Cmd>BufferNext<CR>", { silent = true })
-
--- shaunsingh/nord.nvim
-vim.g.nord_borders = true
-vim.g.nord_contrast = true
-vim.g.nord_italic = false
-
-require("nord").set()
-
--- TODO submit fix
-vim.api.nvim_set_hl(0, "DiffChange", { fg = colors[13] })
-
-vim.api.nvim_set_hl(0, "GitSignsChange", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "GitSignsChangeLn", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "GitSignsChangeNr", { fg = colors[13] })
-
-vim.api.nvim_set_hl(0, "LspDiagnosticsDefaultWarning", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "LspDiagnosticsFloatingWarning", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "LspDiagnosticsSignWarning", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "LspDiagnosticsUnderlineWarning", { sp = colors[13] })
-vim.api.nvim_set_hl(0, "LspDiagnosticsVirtualTextWarning", { fg = colors[13] })
-
-vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = colors[13] })
-
-vim.api.nvim_set_hl(0, "WarningMsg", { fg = colors[13] })
-vim.api.nvim_set_hl(0, "Warnings", { fg = colors[13] })
-
-vim.api.nvim_set_hl(0, "healthWarning", { fg = colors[13] })
 
 -- svermeulen/vim-cutlass
 vim.keymap.set("n", "m", "d")
