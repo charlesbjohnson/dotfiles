20cb8db7297e268aba22829704eae2369f0e7e34
Charles B Johnson <mail@cbjohnson.io>
2022-05-20 11:06:52 -0500

    Relocate plugin settings to personal settings

--- tag-personal/config/nvim/lua/init/personal.lua
+++ tag-personal/config/nvim/lua/init/personal.lua
@@ -18,13 +18,3 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "javascript"
   end,
 })
-
--- AndrewRadev/splitjoin.vim
-vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-
-vim.g.splitjoin_ruby_do_block_split = 0
-vim.g.splitjoin_ruby_hanging_args = 0
-vim.g.splitjoin_ruby_options_as_arguments = 1
-
--- windwp/nvim-autopairs
-require("nvim-autopairs").add_rules(require("nvim-autopairs.rules.endwise-ruby"))
