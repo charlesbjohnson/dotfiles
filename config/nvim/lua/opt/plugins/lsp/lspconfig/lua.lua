b4d1bd34f9885afc4dace848635e04a67259e366
Charles B Johnson <mail@cbjohnson.io>
2022-11-18 22:53:38 -0600

    Silence lua lsp message

--- config/nvim/lua/opt/plugins/lsp/lspconfig/lua.lua
+++ config/nvim/lua/opt/plugins/lsp/lspconfig/lua.lua
@@ -19,6 +19,7 @@ return function(options)
         },
         workspace = {
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         telemetry = {
           enable = false,
