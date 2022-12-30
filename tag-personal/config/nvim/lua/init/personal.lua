93ddc87b789d0ed5191af9412079de732ddce340
Charles B Johnson <mail@cbjohnson.io>
2022-12-29 22:42:15 -0600

    Simplify filetype detection

--- tag-personal/config/nvim/lua/init/personal.lua
+++ tag-personal/config/nvim/lua/init/personal.lua
@@ -1,20 +1,7 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.css.erb" },
-  callback = function()
-    vim.bo.filetype = "eruby.css"
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.html.erb" },
-  callback = function()
-    vim.bo.filetype = "eruby.html"
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.js.erb" },
-  callback = function()
-    vim.bo.filetype = "javascript"
-  end,
+vim.filetype.add({
+  pattern = {
+    [".*%.css%.erb"] = "eruby.css",
+    [".*%.html%.erb"] = "eruby.html",
+    [".*%.js%.erb"] = "javascript",
+  },
 })
