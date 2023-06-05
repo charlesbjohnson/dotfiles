e718e221cae2e9e6bd9483d2ff4a7debaca1992d
Charles B Johnson <mail@cbjohnson.io>
2023-06-05 17:28:32 -0500

    Use yaml formatting builtin

--- config/nvim/lua/opt/plugins/lsp/null-ls/yaml.lua
+++ config/nvim/lua/opt/plugins/lsp/null-ls/yaml.lua
@@ -4,14 +4,7 @@ local M = {}
 
 function M.registration(register)
   register(lspnull.builtins.diagnostics.yamllint)
-  register({
-    filetypes = { "yaml" },
-    method = lspnull.methods.FORMATTING,
-    generator = require("null-ls.helpers").formatter_factory({
-      command = "yq",
-      args = ("--prettyPrint $FILENAME"):split(" "),
-    }),
-  })
+  register(lspnull.builtins.formatting.yq)
 end
 
 return M
